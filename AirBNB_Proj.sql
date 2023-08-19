e2934dce094933308e6e2218578e7733bf96dd51
y-chanyzh <y.chanyzh@booongo.com>
2023-08-19 15:47:21 +0300

    Changed files

--- AirBNB_Proj.sql
+++ AirBNB_Proj.sql
@@ -1,100 +1,72 @@
-CREATE TABLE "users" (
-  "id" integer PRIMARY KEY,
-  "username" varchar,
-  "role" varchar,
-  "created_at" timestamp
+-- Table: users
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  username VARCHAR,
+  role VARCHAR,
+  created_at TIMESTAMP
 );
 
-CREATE TABLE "hosts" (
-  "id" integer,
-  "num_reviews" int,
-  "verified_identity" boolean,
-  "superhost" boolean,
-  "languages" varchar[],
-  "response_rate" decimal,
-  "response_time" varchar
+-- Table: hosts (Many-to-One with users, One-to-Many with rooms)
+CREATE TABLE hosts (
+  id SERIAL,
+  user_id INTEGER REFERENCES users(id),  -- Many-to-One relationship with users
+  num_reviews INT,
+  verified_identity BOOLEAN,
+  superhost BOOLEAN,
+  languages VARCHAR[],
+  response_rate DECIMAL,
+  response_time VARCHAR,
+  PRIMARY KEY (id, user_id)  -- Composite primary key
 );
 
-CREATE TABLE "guests" (
-  "id" integer,
-  "can_pay_for_reservation" boolean,
-  "can_review_host" boolean
+-- Table: guests (Many-to-One with users)
+CREATE TABLE guests (
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER REFERENCES users(id)  -- Many-to-One relationship with users
 );
 
-CREATE TABLE "rooms" (
-  "id" integer PRIMARY KEY,
-  "host_id" integer,
-  "room_name" varchar,
-  "room_description" text,
-  "max_residents" int,
-  "price_per_night" decimal,
-  "has_wifi" boolean,
-  "has_free_parking" boolean,
-  "has_waterfront" boolean,
-  "has_tv_with_cable" boolean,
-  "has_hair_dryer" boolean,
-  "rating" decimal,
-  "common_spaces" varchar[]
+-- Table: rooms (Many-to-One with hosts, One-to-Many with reservations and reviews)
+CREATE TABLE rooms (
+  id SERIAL PRIMARY KEY,
+  host_id INTEGER REFERENCES hosts(id),  -- Many-to-One relationship with hosts
+  room_name VARCHAR,
+  room_description TEXT,
+  max_residents INT,
+  price_per_night DECIMAL,
+  has_wifi BOOLEAN,
+  has_free_parking BOOLEAN,
+  has_waterfront BOOLEAN,
+  has_tv_with_cable BOOLEAN,
+  has_hair_dryer BOOLEAN,
+  rating DECIMAL,
+  common_spaces VARCHAR[],
+  PRIMARY KEY (id, host_id)  -- Composite primary key
 );
 
-CREATE TABLE "reservations" (
-  "id" integer PRIMARY KEY,
-  "guest_id" integer,
-  "room_id" integer,
-  "check_in_date" timestamp,
-  "check_out_date" timestamp,
-  "total_price" decimal,
-  "reservation_status" varchar,
-  "payment_status" varchar
+-- Table: reservations (Many-to-One with guests and rooms)
+CREATE TABLE reservations (
+  id SERIAL PRIMARY KEY,
+  guest_id INTEGER REFERENCES guests(id),  -- Many-to-One relationship with guests
+  room_id INTEGER REFERENCES rooms(id),  -- Many-to-One relationship with rooms
+  check_in_date TIMESTAMP,
+  check_out_date TIMESTAMP,
+  total_price DECIMAL,
+  reservation_status VARCHAR,
+  payment_status VARCHAR
 );
 
-CREATE TABLE "reviews" (
-  "id" integer PRIMARY KEY,
-  "guest_id" integer,
-  "room_id" integer,
-  "cleanliness_rating" int,
-  "accuracy_rating" int,
-  "communication_rating" int,
-  "location_rating" int,
-  "checkin_rating" int,
-  "value_rating" int,
-  "total_rating" decimal,
-  "review_text" text,
-  "review_date" timestamp
+-- Table: reviews (Many-to-One with guests and rooms)
+CREATE TABLE reviews (
+  id SERIAL PRIMARY KEY,
+  guest_id INTEGER REFERENCES guests(id),  -- Many-to-One relationship with guests
+  room_id INTEGER REFERENCES rooms(id),  -- Many-to-One relationship with rooms
+  cleanliness_rating INT,
+  accuracy_rating INT,
+  communication_rating INT,
+  location_rating INT,
+  checkin_rating INT,
+  value_rating INT,
+  total_rating DECIMAL,
+  review_text TEXT,
+  review_date TIMESTAMP
 );
-
-COMMENT ON COLUMN "hosts"."languages" IS 'Array of languages spoken';
-
-COMMENT ON COLUMN "hosts"."response_rate" IS 'Percentage';
-
-COMMENT ON COLUMN "hosts"."response_time" IS 'Response time description';
-
-COMMENT ON COLUMN "rooms"."common_spaces" IS 'Array of common space descriptions';
-
-COMMENT ON COLUMN "reservations"."payment_status" IS 'Pending, Completed, Failed, etc.';
-
-COMMENT ON COLUMN "reviews"."total_rating" IS 'Calculated total rating';
-
-ALTER TABLE "hosts" ADD FOREIGN KEY ("id") REFERENCES "users" ("id");
-
-ALTER TABLE "guests" ADD FOREIGN KEY ("id") REFERENCES "users" ("id");
-
-ALTER TABLE "rooms" ADD FOREIGN KEY ("host_id") REFERENCES "hosts" ("id");
-
-ALTER TABLE "reservations" ADD FOREIGN KEY ("guest_id") REFERENCES "guests" ("id");
-
-ALTER TABLE "reservations" ADD FOREIGN KEY ("room_id") REFERENCES "rooms" ("id");
-
-ALTER TABLE "reviews" ADD FOREIGN KEY ("guest_id") REFERENCES "guests" ("id");
-
-ALTER TABLE "reviews" ADD FOREIGN KEY ("room_id") REFERENCES "rooms" ("id");
-
-ALTER TABLE "rooms" ADD FOREIGN KEY ("id") REFERENCES "hosts" ("id");
-
-ALTER TABLE "reservations" ADD FOREIGN KEY ("id") REFERENCES "guests" ("id");
-
-ALTER TABLE "reservations" ADD FOREIGN KEY ("id") REFERENCES "rooms" ("id");
-
-ALTER TABLE "reviews" ADD FOREIGN KEY ("id") REFERENCES "guests" ("id");
-
-ALTER TABLE "reviews" ADD FOREIGN KEY ("id") REFERENCES "rooms" ("id");
